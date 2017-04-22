b3a312707f37192ce717a3451353dcf144883d86
Victor Fernández <victorferx@gmail.com>
2017-04-22 17:24:30 -0500

    prueba
    prueba de colision

--- automovil.php
+++ automovil.php
@@ -1,5 +1,40 @@
 <?php
-  echo 'automovi.php';
+	echo 'automovi.php';
+	isset($usuario)
+	{
+		echo "Ya existe el usuario";
+	}
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Gestión de usuario</title>
+</head>
+<body>
+	<table>
+		<th>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+		</th>
+		<tr>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+	</table>
+
+
+</body>
+</html>
 
 
