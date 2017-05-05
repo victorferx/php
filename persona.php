ec3df79172519a9f4ba94ae51bd7664386b5a6e9
Victor Fernández <victorferx@gmail.com>
2017-05-04 22:43:52 -0500

    Término de la clase persona
    Se culmina el ejemplo de la clase persona.

--- persona.php
+++ persona.php
@@ -20,7 +20,14 @@ class Persona
 		$this->usuario = $usuario_p;
 
 	}
-
+	public function GetLugarNacimiento()
+	{
+		return $this->lugarNacimiento;
+	}
+	public function SetLugarNacimiento($lugar_p)
+	{
+		$this->lugarNacimiento = $lugar_p;
+	}
 	public function GetSexo()
 	{
 		return $this->sexo;
@@ -30,29 +37,38 @@ class Persona
 		$this->sexo = $sexo_p;
 	}
 
-
-	public function AumentarEdad($incrementoAños)
+	public function GetEdad()
 	{
 		$nacimiento = $this->fechaNacimiento;
 		$hoy = new DateTime();
 		$años = $nacimiento->diff($hoy);
-		printf('%d años, %d meses', $años->y, $años->m);
+		printf('%d años.', $años->y);
+	}
 
-		//return $edad + $incrementoAños;
+	public function AumentarEdad($incrementoAños_p)
+	{
+		$nacimiento = $this->fechaNacimiento;
+		$hoy = new DateTime();
+		$tiempo = $nacimiento->diff($hoy);
+		$edad = $tiempo->y;
+		return $edad + $incrementoAños_p;
 	}
 }
 
-//$victor = new Persona();
 $victor = new Persona('victor123');
 
-
-//$victor.SetSexo('mujer');
+echo('Lugar de nacimiento : ') , $victor->GetLugarNacimiento();
+echo "<br>";
 echo 'El sexo es : ', $victor->GetSexo();
 echo "<br>";
 echo 'Su edad es de : '; 
-$victor->AumentarEdad(2);
-
-
+$victor->GetEdad();
+echo "<br>";
+echo "================================", "<br>";
+echo "Aumentamos la edad 3 años y cambiamos el lugar de nacimiento.", "<br>";
+$victor->SetLugarNacimiento('El Tambo');
+echo "Nuevo lugar de nacimiento : " , $victor->GetLugarNacimiento(), "<br>";
+echo "La edad aumentada en 3 años es : ", $victor->AumentarEdad(3);
  ?>
 
 
